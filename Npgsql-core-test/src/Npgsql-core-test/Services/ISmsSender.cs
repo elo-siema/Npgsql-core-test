a5a3d1bd36a729e7f6198d23c879452811929d19
elochim_siemasz <michal.flak.96@gmail.com>
2016-06-06 09:20:51 +0100

    Packages added

---
+++ Npgsql-core-test/src/Npgsql-core-test/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Npgsql_core_test.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
