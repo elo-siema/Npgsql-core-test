390f89cd6086dad19e97ad29e4f022a5262d4300
elochim_siemasz <michal.flak.96@gmail.com>
2016-06-09 22:09:13 +0100

    Migrations created, test finished

---
+++ Npgsql-core-test/src/Npgsql-core-test/Migrations/20160609203759_testMigration.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql_core_test.Models;
+
+namespace Npgsqlcoretest.Migrations
+{
+    [DbContext(typeof(DomainModelPostgreSqlContext))]
+    [Migration("20160609203759_testMigration")]
+    partial class testMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rc2-20901");
+
+            modelBuilder.Entity("Npgsql_core_test.Models.DataEventRecord", b =>
+                {
+                    b.Property<long>("DataEventRecordId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("SourceInfoId");
+
+                    b.Property<long?>("SourceInfoId1");
+
+                    b.Property<DateTime>("Timestamp");
+
+                    b.Property<DateTime>("UpdatedTimestamp");
+
+                    b.HasKey("DataEventRecordId");
+
+                    b.HasIndex("SourceInfoId1");
+
+                    b.ToTable("DataEventRecord");
+                });
+
+            modelBuilder.Entity("Npgsql_core_test.Models.SourceInfo", b =>
+                {
+                    b.Property<long>("SourceInfoId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.Property<DateTime>("Timestamp");
+
+                    b.Property<DateTime>("UpdatedTimestamp");
+
+                    b.HasKey("SourceInfoId");
+
+                    b.ToTable("SourceInfo");
+                });
+
+            modelBuilder.Entity("Npgsql_core_test.Models.DataEventRecord", b =>
+                {
+                    b.HasOne("Npgsql_core_test.Models.SourceInfo")
+                        .WithMany()
+                        .HasForeignKey("SourceInfoId1");
+                });
+        }
+    }
+}
---
+++ Npgsql-core-test/src/Npgsql-core-test/Migrations/20160609203759_testMigration.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Npgsqlcoretest.Migrations
+{
+    public partial class testMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SourceInfo",
+                columns: table => new
+                {
+                    SourceInfoId = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:Serial", true),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Timestamp = table.Column<DateTime>(nullable: false),
+                    UpdatedTimestamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SourceInfo", x => x.SourceInfoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DataEventRecord",
+                columns: table => new
+                {
+                    DataEventRecordId = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:Serial", true),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    SourceInfoId = table.Column<int>(nullable: false),
+                    SourceInfoId1 = table.Column<long>(nullable: true),
+                    Timestamp = table.Column<DateTime>(nullable: false),
+                    UpdatedTimestamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataEventRecord", x => x.DataEventRecordId);
+                    table.ForeignKey(
+                        name: "FK_DataEventRecord_SourceInfo_SourceInfoId1",
+                        column: x => x.SourceInfoId1,
+                        principalTable: "SourceInfo",
+                        principalColumn: "SourceInfoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataEventRecord_SourceInfoId1",
+                table: "DataEventRecord",
+                column: "SourceInfoId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataEventRecord");
+
+            migrationBuilder.DropTable(
+                name: "SourceInfo");
+        }
+    }
+}
